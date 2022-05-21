94ecfcb9c407a4cb4f2eaf88174c7aefa7625281
jakiiii <me.jaki@outlook.com>
2022-05-21 20:17:04 +0600

    initial commit

---
+++ learnwagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-m6g0(5pal(flq27t9lhg%oh#u^yt&8ljg91o^36t!og*0v@6g^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
